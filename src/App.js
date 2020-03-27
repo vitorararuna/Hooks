3347ca8857158de8bed7d3ebb90248b24c32ca78
vitorararuna <contato.vitorararuna@gmail.com>
2020-03-27 12:11:12 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [tech, setTech] = useState([
-    'ReactJs',
-    'React Native'
-  ])
-
+  const [tech, setTech] = useState([])
   const [newTech, setNewTech] = useState('')
 
 
+
+  //--------------------------------------------------------------------------
   function handleAdd() {
     setTech([...tech, newTech])
-
     setNewTech('');
   }
+  //--------------------------------------------------------------------------
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech')
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []) /* Só vai ser executado uma vez, quando a aplicação for aberta */
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]) /* Vai ser executada quando o state tech for alterado */
+
+  //--------------------------------------------------------------------------
+
+
+
+
+
+
+
 
   return (
     <>
